21ce0494daa680a6aaa281e3ef923bc86299439b
Greg Rose <gregrose628@gmail.com>
2017-10-08 22:22:24 -0400

    removed unused controller, view and route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :translations, only: [:create]
-      resource :simple_translation, only: [:show]
     end
   end
 
