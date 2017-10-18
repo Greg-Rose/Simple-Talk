b1bdc7c44bcde2459f5a01da01256e8b1debd9a8
Greg Rose <gregrose628@gmail.com>
2017-10-18 15:42:11 -0400

    added custom devise_error_messages! helper

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,19 @@
+module DeviseHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+    html = <<-HTML
+    <div class="col-md-12 alert-disappear">
+      <div class="alert alert-danger">
+        <ul>
+          #{messages}
+        </ul>
+      </div>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+end
