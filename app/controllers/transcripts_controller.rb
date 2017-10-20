e8e716e55a1d55b416a83af9cbc6bfe6dbc9f000
Greg Rose <gregrose628@gmail.com>
2017-10-19 20:59:54 -0400

    set order for transcripts in transcripts#index action

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -2,6 +2,6 @@ class TranscriptsController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    @transcripts = current_user.transcripts
+    @transcripts = current_user.transcripts.order(created_at: :desc)
   end
 end
